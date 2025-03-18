a64fb7aede347fcf9a42fd99c86a998655abecd6
Dev_Wonjoon <dnjswns775@gmail.com>
2025-03-18 20:35:45 +0900

    Add: leaveChannel.js

---
+++ commands/music/leaveChannel.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { useQueue } = require('discord-player');
+const { getVoiceConnection } = require('@discordjs/voice');
+
+const data = new SlashCommandBuilder()
+    .setName('나가기')
+    .setDescription('큐를 비우고 음성 채널에서 나갑니다.');
+
+async function execute(interaction) {
+    const queue = useQueue(interaction.guild);
+    const connection = getVoiceConnection(interaction.guild.id);
+
+    if(!connection) {
+        return interaction.reply({ content: "현재 음성 채널에 접속되어 있지 않습니다.", flags: MessageFlags.Ephemeral});
+    }
+
+    connection.destroy();
+    queue.delete();
+    
+}
+
+module.exports = { data, execute }
